72b8a4b36a466da3181a3250a0d66efa2010fbc9
Selcuk Senkul <63095826+mys2web@users.noreply.github.com>
2020-05-07 16:03:02 +0300

    dependency injection

---
+++ src/main/java/guru/springframework/sfgdi/services/PropertyGreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropertyGreetingService implements GreetingService {
+
+    @Override
+    public String sayHello() {
+        return "Hello World - Property";
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/services/SetterGreetingService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingService implements GreetingService {
+
+    @Override
+    public String sayHello() {
+        return "Hello World - Setter";
+    }
+}
